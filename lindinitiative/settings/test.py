827b5819937c632a4906c5e106d5e67415387887
Caroline Simpson <csimpson@cigionline.org>
2015-07-27 11:45:37 -0400

    Adding test settings and requirements.

---
+++ lindinitiative/settings/test.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
