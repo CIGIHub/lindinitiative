3af9b8c33883378757aa7f10871bd93a8b09f50f
Natasha Scott <namscott@gmail.com>
2015-07-31 11:27:02 -0400

    remove javascript effect to fade in overlay and just change general gradient to be darker at the top

--- lindinitiative/static/js/lindinitiative.js
+++ lindinitiative/static/js/lindinitiative.js
@@ -1,12 +1,3 @@
 jQuery(document).ready(function($) {
 
-    $('html').on('click', function() {
-        $('#overlay').fadeIn("slow");
-
-    });
-
-    $(window).on('scroll touchmove', function(){
-        $('#overlay').fadeIn("slow");
-    });
-    
 });
